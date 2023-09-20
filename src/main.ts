f2b9557178d3ca751f2490f0ea7665fec9f4796a
Jacob Gantt <contact@jgantts.com>
2023-09-20 19:31:52 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,67 +2,87 @@ function greeter(person: string) {
   return `Hello, ${person}.`;
 }
  
-let user = "Benny";
+let user = "Monty";
 
 document.body.textContent = greeter(user);
 
-type word_ID = string
-type rule_ID = string
-type ruleGroup_ID = string
-type treeLimb_ID = string
+
+
+type UUID_PhoneFeatureKind = string
+type UUID_Vowel = string
+type UUID_Consonant = string
+type UUID_Syllabic = UUID_Vowel|UUID_Consonant
+
+type UUID_WordPhrase = string
+type UUID_Rule = string
+type UUID_RuleGroup = string
+type UUID_TreeLimb = string
+
+type phoneFeature_Kind = string
+
+type phoneID = string
 
 type langDate = number
 
-type nucleu = {
+type Description = string
 
+type IPA = string
+
+
+
+type PhoneFeatureKind = {
+  id: UUID_PhoneFeatureKind
+  desc: Description
+  stops: { [id: number] : Description; }
 }
+
+type PhoneFeatureValue = number
+
+type Vowel = {
+  id: UUID_Vowel
+  IPA: IPA
+  features: { [id: UUID_PhoneFeatureKind] : PhoneFeatureValue; }
+}
+
+type Consonant = {
+  id: UUID_Consonant
+  IPA: IPA
+  features: { [id: UUID_PhoneFeatureKind] : PhoneFeatureValue; }
+}
+
+type UUID_Cluster = UUID_Consonant[]
+
+type Syllabic = Vowel|Consonant
 
 type Syllable = {
-
-}
-
-interface Sound {
-
-}
-
-interface Consonant extends Sound {
-
-}
-
-interface Vowel extends Sound {
-
-}
-
-type Syllable {
-	
-	subsequent: Syllable|null
+	onset: UUID_Cluster|null
+  rhyme: {
+    nucleus: UUID_Syllabic|null
+    coda: UUID_Cluster|null
+  }
 }
 
 type WordPhrase_Data = {
-  sounds: Syllable
+  sounds: Syllable[]
 }
 
 type WordPhrase = {
-  id: word_ID
-  entryForm: string
-  entryTreeLimb: treeLimb_ID
+  id: UUID_WordPhrase
+  entryForm: Syllable[]
+  entryTreeLimb: UUID_TreeLimb
   entryDate: langDate
-
-  related: word_ID[]
-
-  data: WordPhrase_Data
 }
 
 type Rule = {
-  id: rule_ID
+  id: UUID_Rule
   desc: string
   
 }
 
 type RuleGroup = {
-  id: ruleGroup_ID
+  id: UUID_RuleGroup
   treeLimbs: {
-    limb: treeLimb_ID
+    limb: UUID_TreeLimb
     dates: {
       start: langDate|null
       end: langDate|null
@@ -73,72 +93,192 @@ type RuleGroup = {
 }
 
 type TreeLimb = {
-  id: treeLimb_ID
-  deathDate: langDate
-  children: treeLimb_ID[]
+  id: UUID_TreeLimb
+  branch: {
+    deathDate: langDate
+    children: UUID_TreeLimb[]
+  }
+}
+
+let phones: {
+  vowelFeatures: PhoneFeatureKind[],
+  vowelPhones: Vowel[]
+} = {
+  vowelFeatures: [
+    {
+      id: "QCOTLERJNNSYNKCMMSILQBETHSFYNLLC",
+      desc: "height",
+      stops: {
+        0.0: "open",
+        0.1428: "near-open",
+        0.2857: "open-mid",
+        0.4286: "mid",
+        0.5714: "close-mid",
+        0.8571: "cear-close",
+        1.0: "close",
+      },
+    },
+    {
+      id: "JGJYSLLKLOVLDIKKLQXPDTBRUXNAMGOJ",
+      desc: "backness",
+      stops: {
+        0: "front",
+        0.5: "central",
+        1.0: "back",
+      },
+    },
+    {
+      id: "CTKXTKTLAUXLERTJKSEVLBTSFVYUXSUP",
+      desc: "roundness",
+      stops: {
+        0: "unrounded",
+        1: "rounded",
+      },
+    },
+    {
+      id: "CPZUOQHMIJEUFICZUNGDNWESBMBNUIID",
+      desc: "stress",
+      stops: {
+        0: "unstressed",
+        1: "stressed",
+      },
+    }
+  ],
+  vowelPhones: [
+    {
+      id: "DDQDFQREJUTNLIPBMGAOCGLYUYMGOVJG",
+      IPA: "i",
+      features: {
+        "QCOTLERJNNSYNKCMMSILQBETHSFYNLLC": 1.0,
+        "JGJYSLLKLOVLDIKKLQXPDTBRUXNAMGOJ": 0.0,
+        "CPZUOQHMIJEUFICZUNGDNWESBMBNUIID": 0.0,
+      }
+    },
+    {
+      id: "YCLZGBAVXKMQNDRKIPDZWQDDEBVNFYGX",
+      IPA: "a",
+      features: {
+        "QCOTLERJNNSYNKCMMSILQBETHSFYNLLC": 0.0,
+        "JGJYSLLKLOVLDIKKLQXPDTBRUXNAMGOJ": 0.0,
+        "CPZUOQHMIJEUFICZUNGDNWESBMBNUIID": 0.0,
+      }
+    },
+    {
+      id: "VQWKWCBRGBOAFHSRCRIBGWPDMQPBYIAW",
+      IPA: "u",
+      features: {
+        "QCOTLERJNNSYNKCMMSILQBETHSFYNLLC": 1.0,
+        "JGJYSLLKLOVLDIKKLQXPDTBRUXNAMGOJ": 1.0,
+        "CPZUOQHMIJEUFICZUNGDNWESBMBNUIID": 0.0,
+      }
+    }
+  ],
+}
+
+let lexicon: {
+  words: WordPhrase[],
+  rules: RuleGroup[],
+  treeTrunks: UUID_TreeLimb[],
+  treeLimbs: TreeLimb[],
+} = {
+  words: [
+    {
+      id: "RPYCHUYKXBSWPTMGCDPWDVGDUYRMVAHN",
+      entryForm: [
+        {
+          onset: null,
+          rhyme: {
+            nucleus: "DDQDFQREJUTNLIPBMGAOCGLYUYMGOVJG",
+            coda: null,
+          }
+        },
+      ],
+      entryDate: 0,
+      entryTreeLimb: "QKFUWFZLSQOUHHYTDSMIGWJWJEXLTFYJ",
+    },
+    {
+      id: "GPWQVRUSHHEFXRPYLIHVXFOXKOXXNXZG",
+      entryForm: [
+        {
+          onset: null,
+          rhyme: {
+            nucleus: "DDQDFQREJUTNLIPBMGAOCGLYUYMGOVJG",
+            coda: null,
+          }
+        },
+      ],
+      entryDate: 0,
+      entryTreeLimb: "QKFUWFZLSQOUHHYTDSMIGWJWJEXLTFYJ",
+    },
+  ],
+  rules:  [
+    {
+      id: "ZSUQBWJJCCAHSLUFFSRRANKVNNFHXQOP",
+      treeLimbs: [
+  
+      ],
+  
+    rules: [
+      
+    ],
+    }
+  ],
+  treeTrunks: [
+    "QKFUWFZLSQOUHHYTDSMIGWJWJEXLTFYJ",
+  ],
+  treeLimbs: [
+    {
+      id: "QKFUWFZLSQOUHHYTDSMIGWJWJEXLTFYJ",
+      branch: {
+        deathDate: 100,
+        children: [
+          "LGOQCVJMEHTWMTYYYFKSKSONGNOHZJFP",
+        ],
+      },
+    },
+    {
+      id: "LGOQCVJMEHTWMTYYYFKSKSONGNOHZJFP",
+      branch: {
+        deathDate: 200,
+        children: [
+          "PTTLVWNYQDIEUHUDSSBJGUXOUJFEXJRB",
+          "AUKREWWMFVSHBMHFRNZPXTPVISEGVBFT",
+        ],
+      },
+    },
+    {
+      id: "PTTLVWNYQDIEUHUDSSBJGUXOUJFEXJRB",
+      branch: {
+        deathDate: 300,
+        children: [
+          "VGPMUBSWKOVGVTPGAPBAXKCVLTUVZWMN"
+        ],
+      },
+    },
+    {
+      id: "VGPMUBSWKOVGVTPGAPBAXKCVLTUVZWMN",
+      branch: {
+        deathDate: 400,
+        children: [],
+      },
+    },
+    {
+      id: "AUKREWWMFVSHBMHFRNZPXTPVISEGVBFT",
+      branch: {
+        deathDate: 300,
+        children: [
+          "OIHYZATBJHLDYRSYRHGZGGFVVMLYMVIT",
+        ],
+      },
+    },
+    {
+      id: "OIHYZATBJHLDYRSYRHGZGGFVVMLYMVIT",
+      branch: {
+        deathDate: 400,
+        children: [],
+      },
+    },
+  ],
 }
 
 
-
-let treeTrunks: treeLimb_ID[] = [
-  "QKFUWFZLSQOUHHYTDSMIGWJWJEXLTFYJ",
-]
-
-let treeNodes: TreeLimb[] = [
-  {
-    id: "QKFUWFZLSQOUHHYTDSMIGWJWJEXLTFYJ",
-    deathDate: 100,
-    children: [
-      "LGOQCVJMEHTWMTYYYFKSKSONGNOHZJFP",
-    ],
-  },
-  {
-    id: "LGOQCVJMEHTWMTYYYFKSKSONGNOHZJFP",
-    deathDate: 200,
-    children: [
-      "PTTLVWNYQDIEUHUDSSBJGUXOUJFEXJRB",
-      "AUKREWWMFVSHBMHFRNZPXTPVISEGVBFT",
-    ],
-  },
-  {
-    id: "PTTLVWNYQDIEUHUDSSBJGUXOUJFEXJRB",
-    deathDate: 300,
-    children: [
-      "VGPMUBSWKOVGVTPGAPBAXKCVLTUVZWMN"
-    ],
-  },
-  {
-    id: "VGPMUBSWKOVGVTPGAPBAXKCVLTUVZWMN",
-    deathDate: 400,
-    children: [],
-  },
-  {
-    id: "AUKREWWMFVSHBMHFRNZPXTPVISEGVBFT",
-    deathDate: 300,
-    children: [
-      "OIHYZATBJHLDYRSYRHGZGGFVVMLYMVIT",
-    ],
-  },
-  {
-    id: "OIHYZATBJHLDYRSYRHGZGGFVVMLYMVIT",
-    deathDate: 400,
-    children: [],
-  },
-]
-
-let rules: RuleGroup[] = [
-  {
-    id: "ZSUQBWJJCCAHSLUFFSRRANKVNNFHXQOP",
-    treeLimbs: [
-
-    ],
-
-  rules: [
-    
-  ],
-  }
-]
-
-let words: Lexeme[] = [
-
-]
