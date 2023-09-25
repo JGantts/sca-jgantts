bbf45d227ea35da753a5c01f881248a82ce519a6
Jacob Gantt <contact@jgantts.com>
2023-09-25 18:06:38 -0400

    Can almost drag

--- src/SaveLoad.vue
+++ src/SaveLoad.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref, type Ref } from 'vue';
 import Overview from './Overview.vue';
 import SaveLoad from './SaveLoad.vue'
 import type { SaveFile } from './commonTypes';
@@ -26,6 +27,10 @@ function loadFromFile() {
   if (!input) return
   let files = input.files
   if (!files) return
+  loadFromFiles(files)
+}
+
+function loadFromFiles(files: FileList) {
   const selectedFile = files[0]
   if (!selectedFile) return
   const reader = new FileReader()
@@ -40,12 +45,12 @@ function loadFromFile() {
 
 function saveToFile() {
   let toSave = save()
-  download("mylang.scajg.json", toSave)
+  download("mylang.json", toSave)
 }
 
 
 function load(language: string) {
-
+  console.log(language)
 }
 
 function save(): string {
@@ -69,28 +74,80 @@ function download(filename: string, text: string) {
 
   document.body.removeChild(element);
 }
+
+let isDragging = ref(false)
+
+function dragover(e: DragEvent) {
+  e.stopImmediatePropagation();
+  e.preventDefault();
+  isDragging.value = true
+  console.log("over")
+  document.body.style.cursor = "default"
+}
+
+function dragleave(e: DragEvent) {
+  e.stopImmediatePropagation();
+  e.preventDefault();
+  console.log(`leave: '${(e.target as HTMLElement).innerHTML}'`)
+  isDragging.value = false
+  document.body.style.cursor = "default"
+}
+
+function drop(e: DragEvent) {
+  e.stopImmediatePropagation();
+  e.preventDefault();
+  isDragging.value = false
+
+  const dt = e.dataTransfer;
+  const files = dt?.files;
+  if (files) {
+    loadFromFiles(files);
+  }
+}
 </script>
 
 <template>
-  <div class="container">
-    <h3>Load</h3>
-    <div>
-      <button @click="loadTemplate('ipa')">
-        Load IPA template
-      </button>
-      &nbsp;
-      <button @click="loadTemplate('blank')">
-        Load blank template
-      </button>
+  <div
+    id="drag-container1"
+    style="position:relative;"
+    @dragover="dragover"
+    @dragleave="dragleave"
+    @drop="drop"
+  >
+    <div
+    id="drag-container2"
+      class="container"
+    >
+      <h3>Load</h3>
+      <div>
+        <button @click="loadTemplate('ipa')">
+          Load IPA template
+        </button>
+        &nbsp;
+        <button @click="loadTemplate('blank')">
+          Load blank template
+        </button>
+      </div>
+      <br />
+      <div>
+        <input 
+          type="file"
+          id="fileToLoad"
+          accept=".json"
+        />
+      </div>
+      <div>
+        <button @click="loadFromFile()">
+          Load from chosen .json file
+        </button>
+      </div>
     </div>
-    <br />
-    <div>
-      <input type="file" id="fileToLoad" accept=".scajg.json"/>
-    </div>
-    <div>
-      <button @click="loadFromFile()">
-        Load from chosen .scajg.json file
-      </button>
+    <div
+    class="draggingOverlay"
+    :class="{ isDraggingOverlay: isDragging }"
+    id="drag-container3"
+    >
+      Drop Project File Here
     </div>
   </div>
   <br />
@@ -98,7 +155,7 @@ function download(filename: string, text: string) {
     <h3>Save</h3>
     <div>
       <button @click="saveToFile()">
-        Save to .scajg.json file
+        Save to .json file
       </button>
     </div>
   </div>
@@ -121,5 +178,23 @@ function download(filename: string, text: string) {
 
 .container > h3 {
   padding-bottom: 0.75rem;
+}
+
+.draggingOverlay {
+  position: absolute;
+  top:0;
+  left:0; 
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background: radial-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.25));
+  border-radius: 1rem;
+  color: white;
+  display: none;
+}
+
+.isDraggingOverlay {
+  display: flex;
 }
 </style>
