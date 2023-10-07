52540ac614bfc1a6f265224c7615d9ae425612b2
Jacob Gantt <contact@jgantts.com>
2023-10-07 01:52:59 -0400

    Add Vuetify

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,22 @@ import { createPinia } from 'pinia';
 import VNetworkGraph from "v-network-graph"
 import "v-network-graph/lib/style.css"
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const pinia = createPinia()
 
 let app = createApp(App)
     .component('tabs', Tabs)
     .component('tab', Tab)
+    .use(vuetify)
     .use(pinia)
     .use(VNetworkGraph)
     .mount('#app')
