bdd421ee7794c820c11820ce23c3e178f7697e09
Jacob Gantt <contact@jgantts.com>
2023-10-09 17:53:06 -0400

    Publish

--- docs/assets/example-store.3a312324.js
+++
@@ -1,1 +0,0 @@
-import{a1 as n}from"./index.a1cfdcbd.js";const t=[],o=[],a=n("languages",{state:()=>({languages:null,currentTab:"loadsave"}),actions:{loadSaveFile(e){e?this.languages=e:this.languages=null},excuteUndo(){const e=t.pop();!e||(e.undo(),o.push(e))},excuteRedo(){const e=o.pop();!e||(e.do(),t.push(e))},executeDo(e){e.do(),t.push(e)}}});export{a as u};
