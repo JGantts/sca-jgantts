f2b50bfd3871ead4b5519ff3491db1aa70b861ac
Jacob Gantt <contact@jgantts.com>
2023-10-10 18:58:02 -0400

    Publish

--- docs/assets/languages-store.5cdfddf8.js
+++
@@ -1,1 +0,0 @@
-import{ag as n}from"./index.e96ee7c1.js";const t=[],o=[],a=n("languages",{state:()=>({languages:null,currentTab:"loadsave"}),actions:{loadSaveFile(e){e?this.languages=e:this.languages=null},excuteUndo(){const e=t.pop();!e||(e.undo(),o.push(e))},excuteRedo(){const e=o.pop();!e||(e.do(),t.push(e))},executeDo(e){e.do(),t.push(e)}}});export{a as u};
