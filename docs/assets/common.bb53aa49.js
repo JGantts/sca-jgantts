24af61a10d143e86de8dd4cad99645296c8d1195
Jacob Gantt <contact@jgantts.com>
2023-10-13 22:21:26 -0400

    Publish

--- docs/assets/common.bb53aa49.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index.4bbe145e.js";import{_ as a}from"./index.20ccf6e0.js";var s=r(({app:o})=>{o.use(a)});export{s as default};
---
+++ docs/assets/languages-store.958e9885.js
@@ -0,0 +1,1 @@
+import{ag as n}from"./index.9ef23736.js";const t=[],o=[],a=n("languages",{state:()=>({languages:null,currentTab:"loadsave"}),actions:{loadSaveFile(e){e?this.languages=e:this.languages=null},excuteUndo(){const e=t.pop();!e||(e.undo(),o.push(e))},excuteRedo(){const e=o.pop();!e||(e.do(),t.push(e))},executeDo(e){e.do(),t.push(e)}}});export{a as u};
